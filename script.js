f613ba6fedc534284bc7a4e0e4714d528b23ead8
ivnteterin <spy930712@gmail.com>
2021-10-31 18:08:45 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+//SHOP STATUS (CLOSED / OPEN)
+
+function reqListener() {
+  let week;
+  let hours;
+  let minutes;
+
+  const options = {
+    timeZone: 'Europe/Vilnius',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
+  const date = new Date();
+  const formatter = new Intl.DateTimeFormat('lt', options);
+  const time = formatter.format(date);
+
+  week = date.getDay();
+  hours = time.substring(0, 2);
+  minutes = time.substring(3, 5);
+
+  const closed = document.getElementById('closed');
+  const open = document.getElementById('open');
+  if (
+    (week !== 0 && week < 7 && hours < 18 && hours >= 9) ||
+    (week === 0 && hours < 16 && hours >= 10)
+  ) {
+    if (
+      !open.classList.contains('hidden') &&
+      closed.classList.contains('hidden')
+    ) {
+      return;
+    }
+    open.classList.remove('hidden');
+    closed.classList.add('hidden');
+  } else {
+    if (
+      !closed.classList.contains('hidden') &&
+      open.classList.contains('hidden')
+    ) {
+      return;
+    }
+    closed.classList.remove('hidden');
+    open.classList.add('hidden');
+  }
+}
+
+function loadShopStatus() {
+  reqListener();
+}
+
+loadShopStatus();
+setInterval(loadShopStatus, 60000);
