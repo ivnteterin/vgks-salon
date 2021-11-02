822958805e4e67298ef9ddefb31c3aa38bf8a9e6
ivnteterin <spy930712@gmail.com>
2021-11-02 11:30:29 +0200

    fix absolute pos

--- script.js
+++ script.js
@@ -61,8 +61,9 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// window.addEventListener('resize', () => {
-//   // We execute the same script as before
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  if (window.innerWidth < 767 && window.innerHeight < 500) return;
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
