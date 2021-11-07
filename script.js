b3f24315c3cb5f7a687c270815487857357344e0
ivnteterin <spy930712@gmail.com>
2021-11-07 13:02:53 +0200

    fix absolute pos

--- script.js
+++ script.js
@@ -57,13 +57,21 @@ setInterval(loadShopStatus, 60000);
 // VIEWPORT FIX FOR MOBILE
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
+// let vh = window.innerHeight * 0.01;
+let prevVh = window.innerHeight * 0.01;
+let prevVw = window.innerWidth * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${prevVh}px`);
 
 window.addEventListener('resize', () => {
-  // We execute the same script as before
+  const currVw = window.innerWidth * 0.01;
+
+  if (currVw < prevVw) {
+    document.documentElement.style.setProperty('--vh', `${prevVh}px`);
+  }
   if (window.innerWidth < 550 && window.innerHeight < 768) return;
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  prevVh = window.innerHeight * 0.01;
+  prevVw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--vh', `${prevVh}px`);
 });
