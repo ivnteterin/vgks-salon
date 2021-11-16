c2d7a98f208673b20734d341779ac542e2345363
ivnteterin <spy930712@gmail.com>
2021-11-17 01:58:56 +0200

    fix

--- gallery.js
+++ gallery.js
@@ -1,39 +1,54 @@
-var openPhotoSwipe = function () {
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+// var openPhotoSwipe = function () {
+//   var pswpElement = document.querySelectorAll('.pswp')[0];
 
-  // build items array
-  var items = [
-    {
-      src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
-      w: 964,
-      h: 1024,
-    },
-    {
-      src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
-      w: 1024,
-      h: 683,
-    },
-  ];
+//   // build items array
+//   var items = [
+//     {
+//       src: '/img/gallery/nail-1.jpg',
+//       w: 1877,
+//       h: 1250,
+//     },
+//     {
+//       src: '/img/gallery/nail-2.jpg',
+//       w: 1000,
+//       h: 1250,
+//     },
+//     {
+//       src: '/img/gallery/nail-3.jpg',
+//       w: 1000,
+//       h: 1250,
+//     },
+//   ];
 
-  // define options (if needed)
-  var options = {
-    // history & focus options are disabled on CodePen
-    history: false,
-    focus: false,
+//   // define options (if needed)
+//   var options = {
+//     // history & focus options are disabled on CodePen
+//     history: false,
+//     focus: false,
 
-    showAnimationDuration: 0,
-    hideAnimationDuration: 0,
-  };
+//     showAnimationDuration: 0,
+//     hideAnimationDuration: 0,
+//   };
 
-  var gallery = new PhotoSwipe(
-    pswpElement,
-    PhotoSwipeUI_Default,
-    items,
-    options
-  );
-  gallery.init();
+//   var gallery = new PhotoSwipe(
+//     pswpElement,
+//     PhotoSwipeUI_Default,
+//     items,
+//     options
+//   );
+//   gallery.init();
+// };
+
+// openPhotoSwipe();
+
+// document.querySelector('.gallery__item').onclick = openPhotoSwipe;
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
+
+const options = {
+  gallerySelector: '#my-gallery',
+  childSelector: 'a',
+  pswpModule: './photoswipe.esm.js',
 };
 
-openPhotoSwipe();
-
-document.getElementById('btn').onclick = openPhotoSwipe;
+const lightbox = new PhotoSwipeLightbox(options);
+lightbox.init();
