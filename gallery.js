0f99462ab82fa507ffd5e72cc7c89abcdcb32885
ivnteterin <spy930712@gmail.com>
2021-12-27 23:24:50 +0200

    fix

--- gallery.js
+++ gallery.js
@@ -3,8 +3,7 @@ import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js';
 const options = {
   gallerySelector: '#my-gallery',
   childSelector: 'a',
-  pswpModule:
-    'https://raw.githack.com/dimsemenov/PhotoSwipe/v5-beta/dist/photoswipe.esm.js',
+  pswpModule: '/photoswipe.esm.js',
 };
 
 const lightbox = new PhotoSwipeLightbox(options);
